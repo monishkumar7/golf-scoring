0a6b5ad641f9f1e64356806354d378c600492abe
monishkumar <monishkumar1154@gmail.com>
2018-08-07 06:38:52 -0400

    Add login page validations

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,38 +2,110 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import classes from "./Login.css";
-import TextBox from "../UI/TextBox/TextBox";
+import FormInput from "../UI/FormInput/FormInput";
 import Button from "../UI/Button/Button";
 import * as actionCreators from "../../store/actions";
 
-//FIXME:
-//1. Proper Login Flow
-
 class Login extends Component {
   state = {
-    email: "",
-    password: ""
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Enter your Allcal Email Address"
+        },
+        validation: {
+          required: true
+        },
+        value: "",
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: "password",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password"
+        },
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        value: "",
+        valid: false,
+        touched: false
+      }
+    }
   };
 
-  emailChange = event => {
-    this.setState({ email: event.target.value });
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        touched: true,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        )
+      }
+    };
+    this.setState({ controls: updatedControls });
   };
 
-  passwordChange = event => {
-    this.setState({ password: event.target.value });
-  };
-
-  loginClick = () => {
-    this.props.onLoginClick(this.state.email, this.state.password);
+  submitHandler = event => {
+    event.preventDefault();
+    this.props.onLoginClick(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    );
   };
 
   render() {
+    const formElements = [];
+    for (let key in this.state.controls) {
+      formElements.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+
+    let form = formElements.map(formElement => (
+      <FormInput
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={event => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+
     return (
       <div className={classes.Login}>
-        Login Page
-        <TextBox type="text" name="email" changed={this.emailChange} />
-        <TextBox type="text" name="password" changed={this.passwordChange} />
-        <Button clicked={this.loginClick}>Login</Button>
+        <h4>RB Golf Scoring</h4>
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button>SUBMIT</Button>
+        </form>
       </div>
     );
   }
